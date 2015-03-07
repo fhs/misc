d15c8d167691e5bddabf3ab77fe9d46a69d2e37a
Fazlul Shahriar <fshahriar@gmail.com>
2015-03-06 19:08:53 -0500

    cmd/goplay: add new command

---
+++ cmd/goplay/main.go
@@ -0,0 +1,90 @@
+// Copyright 2015 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Modified from https://github.com/9fans/go/blob/master/acme/editinacme/main.go
+
+// Goplay is a Go playground for acme(1).
+//
+// Goplay uses the plumber to ask acme to open a temporary file,
+// and runs the file everytime Put is executed for that file. Once
+// the file's acme window is deleted, it removes the temporary file,
+// and exits.
+//
+// Run with acmego (http://godoc.org/code.google.com/p/rsc/cmd/acmego)
+// for import path rewriting.
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+
+	"9fans.net/go/acme"
+)
+
+var HelloProg = `package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, 世界")
+}
+`
+
+func writeHello(filename string) error {
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	f.WriteString(HelloProg)
+	return f.Close()
+}
+
+func run(filename string) {
+	cmd := exec.Command("go", "run", filename)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Printf("%s\n", err)
+	}
+}
+
+func main() {
+	log.SetFlags(0)
+	log.SetPrefix("goplay: ")
+
+	dir, err := ioutil.TempDir("", "goplay")
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	defer os.RemoveAll(dir)
+	file := path.Join(dir, "a.go")
+	writeHello(file)
+
+	r, err := acme.Log()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	out, err := exec.Command("plumb", "-d", "edit", file).CombinedOutput()
+	if err != nil {
+		log.Fatalf("executing plumb: %v\n%s", err, out)
+	}
+
+	for {
+		ev, err := r.Read()
+		if err != nil {
+			log.Fatalf("reading acme log: %v", err)
+		}
+		if ev.Op == "del" && ev.Name == file {
+			break
+		}
+		if ev.Op == "put" && ev.Name == file {
+			run(file)
+		}
+	}
+}
